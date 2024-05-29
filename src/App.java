64361f60f2ef223072a0f9bb6e4d123f69c6074a
Keyaru <118556238+keyaru18@users.noreply.github.com>
2024-05-28 20:07:26 -0500

    Agregar serie de caracteres del 3 al 10

--- src/App.java
+++ src/App.java
@@ -2,15 +2,34 @@
 *richard.tipantiza@epn.edu.ec
 *Version 1.2
 *Richard Tipantiza*/ 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
         System.out.println("Prj_GRUPO4");
         System.out.println("Integrantes: Sebastian, Richard, Carlos, Rommel");
-        
-        TipantizaRichard tr = new TipantizaRichard(7, "+", "-");
 
-        tr.trSC1();
-        tr.trSC2();
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Introduce el número de términos/caracteres a usar: ");
+            int size = sc.nextInt();
+            TipantizaRichard tr = new TipantizaRichard(size);
+            tr.trSC1();
+            tr.trSC2();
+            tr.trSC3();
+            tr.trSC4();
+            tr.trSC5();
+            tr.trSC6();
+            tr.trSC7();
+            tr.trSC8();
+            tr.trSC10();
+            tr.trSC9();
+            
+        } catch (Exception e) {
+            System.out.println("Error pe causa: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            sc.close();
+        }
     }
 }
--- src/TipantizaRichard.java
+++ src/TipantizaRichard.java
@@ -1,54 +1,164 @@
-public class TipantizaRichard{
-    int size;
-    String caracter1;
-    String caracter2;
-    //constructor
-    public TipantizaRichard(Integer size, String caracter1, String caracter2){
-        this.size       = (size!=null)? size: 0;
-        this.caracter1  = (caracter1!=null)? caracter1: "";
-        this.caracter2  = (caracter2!=null)? caracter2: "";
+public class TipantizaRichard {
+    private int size;
+
+    public TipantizaRichard(int size) {
+        this.size = size;
     }
-
-
-    //metodo : +-+-+-+- ...
-    public void trSC1(){
-        System.out.println ("FOR: ");
-        for (int i = 1; i <= size; i++){
-            if (i % 2 ==0)
-                System.out.print(caracter2 + " ");
-            else
-                System.out.print(caracter1 + " ");
-        }
+    // metodo : + - + - + - + - ...
+    public void trSC1() {
         System.out.println("");
+        char caracter1 = '+';
+        char caracter2 = '-';
+        for (int i = 1; i <= size; i++) {
+            System.out.print((i % 2 == 0) ? caracter2 : caracter1);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
-    
-    //metodo : + + ++ +++ +++++ ++++++++ +++++++++++++ ...
-    public void trSC2(){
-        System.out.println ("FOR: ");
-        int f1=0;
-        int f2=1;
+
+    //  + + ++ +++ +++++ ++++++++ +++++++++++++ ...
+    public void trSC2() {
+        System.out.println("");
+        char caracter1 = '+';
+        int f1 = 0;
+        int f2 = 1;
         int fb;
         String resp;
-        for (int i = 1; i <= size; ++i){
-            if (i==1){
-                resp =caracter1;
-            }else{ 
-                fb=f1+f2;
-                f1=f2;
-                f2=fb;
-                resp= repeat(caracter1, fb);
+        for (int i = 1; i <= size; ++i) {
+            if (i == 1) {
+                resp = Character.toString(caracter1);
+            } else {
+                fb = f1 + f2;
+                f1 = f2;
+                f2 = fb;
+                resp = repeat(Character.toString(caracter1), fb);
             }
             System.out.print(resp + " ");
         }
         System.out.println("");
     }
 
-    private String repeat(String str, int times) {
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < times; i++) {
-        result.append(str);
+    // ++ +++ +++++ +++++++ +++++++++++
+    public void trSC3() {
+        System.out.println("");
+        char caracter1 = '+';
+        for (int i = 2; i <= size; i++) {
+            if (esPrimo(i)) {
+                System.out.print(repeat(Character.toString(caracter1), i) + " ");
+            }
         }
-    return result.toString();
+        System.out.println("");
     }
 
+    // + - * / + - * / + - * / ...
+    public void trSC4() {
+        System.out.println("");
+        char[] caracteres = {'+', '-', '*', '/'};
+        for (int i = 0; i < size * 4; i++) {
+            System.out.print(caracteres[i % 4] + " ");
+        }
+        System.out.println("");
+    }
+
+    // \ | / -  |  \ | / -  |  \ | / -  | ...
+    public void trSC5() {
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" \\ | / -  |");
+        }
+        System.out.println("");
+    }
+
+    // a b c d e f g h i j ...
+    public void trSC6() {
+        System.out.println("");
+        for (char letra = 'a'; letra < 'a' + size; letra++) {
+            System.out.print(letra + " ");
+        }
+        System.out.println("");
+    }
+
+    // a + c - e + g ...
+    public void trSC7() {
+        System.out.println("");
+        char caracter1 = '+';
+        char caracter2 = '-';
+        if (size > 14) {
+            System.out.println("Se acabó el abecedario crack!!");
+        } else {
+            for (int i = 0; i < size; i++) {
+                System.out.print((char)('a' + i * 2) + " ");
+                System.out.print((i % 2 == 0 ? caracter1 : caracter2) + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    // aa bbbb cccccc dddddddd ...
+    public void trSC8() {
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            char letra = (char) ('a' + i);
+            int repeticiones = (i + 1) * 2;
+            for (int j = 0; j < repeticiones; j++) {
+                System.out.print(letra);
+            }
+            System.out.print(" ");
+        }
+        System.out.println("");
+    }
+
+
+    // a b c dd eee fffff gggggggg ...
+    public void trSC9() {
+        System.out.println("");
+        int f1 = 1;
+        int f2 = 1;
+        for (int i = 0; i < size; i++) {
+            char letra = (char) ('a' + i);
+            for (int j = 0; j < f2; j++) {
+                System.out.print(letra);
+            }
+            System.out.print(" ");
+            int temp = f2;
+            f2 = f1 + f2;
+            f1 = temp;
+        }
+        System.out.println();
+    }
+
+    // a bbb ccccc ddddddd eeeeeeeee ...
+    public void trSC10() {
+    System.out.println("");
+    for (int i = 0; i < size; i++) {
+        char letra = (char) ('a' + i);
+        int repeticiones = i * 2 + 1; 
+        for (int j = 0; j < repeticiones; j++) {
+            System.out.print(letra);
+        }
+        System.out.print(" ");
+    }
+    System.out.println("");
+}
+
+    // // Métodos auxiliares
+    private boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String repeat(String str, int times) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            result.append(str);
+        }
+        return result.toString();
+    }
 }
